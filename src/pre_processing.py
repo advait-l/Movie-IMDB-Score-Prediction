2188c6b36b4314a351543e0dbf694e86c3a26c83
gandharv <gandharv.suri@iiitb.org>
2019-11-23 17:40:04 +0530

    content_raing

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -10,8 +10,8 @@ def Encoder(df):
     labelencoder = LabelEncoder()
     df['color'] = labelencoder.fit_transform(df['color'])
     print(df.head(10))
-    onehotencoder = OneHotEncoder(categorical_features=[1])
-    df = onehotencoder.fit_transform(df).toarray()
+    #onehotencoder = OneHotEncoder(categorical_features=[])
+    #df = onehotencoder.fit_transform(df).toarray()
     return df
 
 def remove_duplicates(df):
@@ -23,37 +23,43 @@ def fillNullValues(df):
     df.color.fillna(value="unknown", inplace=True)
     df.director_name.fillna(value="unknown", inplace=True)
 
-    
     #numeric_features = df._get_numeric_data().columns.values.tolist()
     #numeric_features.remove("title_year")
-    
-
-
     #numeric_features = df._get_numeric_data().columns.values.tolist()
     #numeric_features.remove("title_year")
-
-
     # Imputer : Fill the missing values with the median value for all the numeric features.
     #imp=SimpleImputer(missing_values = np.nan,strategy="median")
     #imp = imp.fit(df[numeric_features])
     #df[numeric_features] = imp.transform(df[numeric_features])
-
     # Standard Scaler : Scale all the numeric values to the range of values such that mean = 0 and variance = 1
     #scl=StandardScaler()
     #df[numeric_features]=scl.fit_transform(df[numeric_features])
     return df
 
+
+def rating(df):
+    df['content_rating'].replace(to_replace = ['M', 'GP', 'TV-PG','TV-Y7'] , value = 'PG', inplace = True)
+    df['content_rating'].replace(to_replace = ['X'] , value = 'NC-17', inplace = True)
+    df['content_rating'].replace(to_replace = ['Approved','Not Rated', 'Passed', 'Unrated','TV-MA'] , value = 'R', inplace = True)
+    df['content_rating'].replace(to_replace = ['TV-G','TV-Y'] , value = 'G', inplace = True)
+    df['content_rating'].replace(to_replace = 'TV-14' , value = 'PG-13', inplace = True)
+
+    return df
 if __name__ == "__main__":
-    df = pd.read_csv('../movie_metadata/processed_data.csv')
-    #print(df.isnull().sum())
-    #print(df.info())
-    df = fillNullValues(df)
-    #print("###########################################################################################")
-    #print(df.isnull().sum())
+    
+    #When Running for the first time
+    #df = pd.read_csv('movie_metadata/movie_metadata.csv')
+
+    #When updating the code
+    df = pd.read_csv('movie_metadata/processed_data.csv')
+    
+    #df = fillNullValues(df)
     #df = remove_duplicates(df)
-    df = Encoder(df)
+    #df = Encoder(df)
+    #df = drop(df)
+    df = rating(df)
     print(df)
-
-
-
-    #df.to_csv('../movie_metadata/processed_data.csv')
+    x = df.content_rating.unique()
+    print(x)
+    print(len(x))
+    df.to_csv('movie_metadata/processed_data.csv')
