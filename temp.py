71022129437fe0bd0de7da64fe758f129a1f8805
gandharv <gandharv.suri@iiitb.org>
2019-11-23 19:23:10 +0530

    Encoding

--- temp.py
+++ temp.py
@@ -34,39 +34,39 @@ def handlingValues(df):
     # Profit column
     df["profit"] = df["gross"] - df["budget"]
 
-    return df
-
-def remove_duplicates(df):
-    df.drop_duplicates(subset="movie_title", keep= 'first', inplace = True)
-    return df
-
-def ContentRating(df):
     df['content_rating'].replace(to_replace = ['M', 'GP', 'TV-PG','TV-Y7'] , value = 'PG', inplace = True)
     df['content_rating'].replace(to_replace = ['X'] , value = 'NC-17', inplace = True)
     df['content_rating'].replace(to_replace = ['Approved','Not Rated', 'Passed', 'Unrated','TV-MA'] , value = 'R', inplace = True)
     df['content_rating'].replace(to_replace = ['TV-G','TV-Y'] , value = 'G', inplace = True)
     df['content_rating'].replace(to_replace = 'TV-14' , value = 'PG-13', inplace = True)
     df['content_rating'].fillna('R',inplace = True)
+
+    return df
+
+def remove_duplicates(df):
+    df.drop_duplicates(subset="movie_title", keep= 'first', inplace = True)
+    return df
+
+def encoding(df):
     
     labelencoder = LabelEncoder()
     df['content_rating'] = labelencoder.fit_transform(df['content_rating'])
 
-    print(df)
-    i = df.columns.get_loc('content_rating') 
-    #onehotencoder = OneHotEncoder(categorical_features=[i])
-    #df = onehotencoder.fit_transform(df).toarray()
-
     df = pd.get_dummies(df, columns=['content_rating'])
+
+    df['country'] = labelencoder.fit_transform(df['country'])
+
+    df = pd.get_dummies(df, columns=['country'])
 
     return df
 
 if __name__ == "__main__":
     df = pd.read_csv("movie_metadata/movie_metadata.csv")
-    print(df.isnull().sum())
+    #print(df.isnull().sum())
     #print(df.info())
     df = handlingValues(df)
     df = remove_duplicates(df)
-    df = ContentRating(df)
+    df = encoding(df)
     print(df.isnull().sum())
     print(df)
     df.to_csv('movie_metadata/processed_data.csv')
