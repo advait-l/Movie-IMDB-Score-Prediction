70169218c0e20c58eb941c42e6bc3a69d697d514
Mili Goyal <goyal9mili@gmail.com>
2019-11-23 14:42:27 +0530

    pairplots

--- src/visualization.py
+++ src/visualization.py
@@ -9,6 +9,21 @@ def imdbScoreHistogram(df):
     plt.title("Distribution of the IMDB ratings")
     plt.savefig("visualizations/IMDB-Score-Histogram.png")
 
+def pairplot(df2):
+
+    sns.pairplot(df2).savefig("visualizations/pairplots.png")
+    # print( df.head())
+    # sns.pairplot(data = df[["gross", "aspect_ratio"]], hue="Survived", dropna=True)
+        # sns.pairplot(df)
+def pairplots_specific(df2):
+
+    sns.pairplot(df2, vars = ["gross", "aspect_ratio"]).savefig("visualizations/pairplot1.png")
+
+
 if __name__ == "__main__":
     df = pd.read_csv("movie_metadata/movie_metadata.csv")
+    df2 = pd.read_csv("movie_metadata/movie_metadata.csv")
+    df2.dropna(inplace = True)
     imdbScoreHistogram(df)
+    pairplot(df2)
+    pairplots_specific(df2)
