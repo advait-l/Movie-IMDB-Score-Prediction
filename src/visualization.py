d97bed39770cf49b607262c156c4d5d001ef488c
Mili Goyal <goyal9mili@gmail.com>
2019-11-23 15:47:26 +0530

    correlation_matrix

--- src/visualization.py
+++ src/visualization.py
@@ -14,16 +14,24 @@ def pairplot(df2):
     sns.pairplot(df2).savefig("visualizations/pairplots.png")
     # print( df.head())
     # sns.pairplot(data = df[["gross", "aspect_ratio"]], hue="Survived", dropna=True)
-        # sns.pairplot(df)
+    #sns.pairplot(df)
 def pairplots_specific(df2):
 
     sns.pairplot(df2, vars = ["gross", "aspect_ratio"]).savefig("visualizations/pairplot1.png")
 
+def correlation_matrix(df2):
+    corr = df2.corr()
+    sns.set(context = "paper", font = "monospace")
+    svm = sns.heatmap(corr)
+    figure = svm.get_figure()
+    figure.savefig("visualizations/corr_matrix.png")
 
 if __name__ == "__main__":
     df = pd.read_csv("movie_metadata/movie_metadata.csv")
     df2 = pd.read_csv("movie_metadata/movie_metadata.csv")
     df2.dropna(inplace = True)
-    imdbScoreHistogram(df)
-    pairplot(df2)
-    pairplots_specific(df2)
+    # imdbScoreHistogram(df)
+    # pairplot(df2)
+    # pairplots_specific(df2)
+    correlation_matrix(df2)
+    # print(sum(duplicated(df)))
