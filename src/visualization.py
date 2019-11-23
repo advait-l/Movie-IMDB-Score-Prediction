074e652071c6e5ef1c682b126557e5ad32bd3d5d
advait-l <lonkaradvait@gmail.com>
2019-11-23 15:49:40 +0530

    lol

--- src/visualization.py
+++ src/visualization.py
@@ -8,7 +8,6 @@ def imdbScoreHistogram(df):
     plt.hist(labels, bins=20)
     plt.title("Distribution of the IMDB ratings")
     plt.savefig("visualizations/IMDB-Score-Histogram.png")
-    sns.pairplot(df)
 
 def pairplot(df2):
 
@@ -20,11 +19,17 @@ def pairplots_specific(df2):
 
     sns.pairplot(df2, vars = ["gross", "aspect_ratio"]).savefig("visualizations/pairplot1.png")
 
+def correlation_matrix(df2):
+    corr = df2.corr()
+    svm = sns.heatmap(corr)
+    figure = svm.get_figure()
+    figure.savefig("visualizations/corr_matrix.png")
 
 if __name__ == "__main__":
     df = pd.read_csv("movie_metadata/movie_metadata.csv")
     df2 = pd.read_csv("movie_metadata/movie_metadata.csv")
     df2.dropna(inplace = True)
-    imdbScoreHistogram(df)
-    pairplot(df2)
-    pairplots_specific(df2)
+    #imdbScoreHistogram(df)
+    #pairplot(df2)
+    #pairplots_specific(df2)
+    correlation_matrix(df2)
