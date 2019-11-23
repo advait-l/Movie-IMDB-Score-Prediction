d9b7aed7d4d87283d1c2d0cfb9f8cb93dba43b40
advait-l <lonkaradvait@gmail.com>
2019-11-23 12:02:56 +0530

    numeric values null values handled

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -3,18 +3,21 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler 
 
+
 def fillNullValues(df):
+    # Fill "unknown" for missing values of color and director_name
     df.color.fillna(value="unknown", inplace=True)
     df.director_name.fillna(value="unknown", inplace=True)
+    
     numeric_features = df._get_numeric_data().columns.values.tolist()
     numeric_features.remove("title_year")
-
-    # Imputer
+    
+    # Imputer : Fill the missing values with the median value for all the numeric features.
     imp=SimpleImputer(missing_values = np.nan,strategy="median")
     imp = imp.fit(df[numeric_features])
     df[numeric_features] = imp.transform(df[numeric_features])
 
-    # Standard Scaler
+    # Standard Scaler : Scale all the numeric values to the range of values such that mean = 0 and variance = 1
     scl=StandardScaler()
     df[numeric_features]=scl.fit_transform(df[numeric_features])
     return df
@@ -26,4 +29,5 @@ if __name__ == "__main__":
     df = fillNullValues(df)
     print("###########################################################################################")
     print(df.isnull().sum())
+    print(df)
 
