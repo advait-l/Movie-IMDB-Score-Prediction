d137020f0bf3a23b2cdb5d7651cfd20d1ef09a06
gandharv <gandharv.suri@iiitb.org>
2019-11-24 20:00:01 +0530

    PolyReg

--- src/model.py
+++ src/model.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
 
 def linear_regression(x1,y1,x2,y2):
     regr = linear_model.LinearRegression()
@@ -13,12 +16,30 @@ def linear_regression(x1,y1,x2,y2):
     print("Mean-squared-error : " + str(mean_squared_error(y2, imdb_predict)))
     print("Variance : " + str(r2_score(y2, imdb_predict)))
 
+
+def PolyReg(df):
+    y = df.imdb_score.values
+    X = df.drop(["imdb_score","movie_title"], axis = 1)
+    X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.2)
+    
+    lin2 = LinearRegression()
+
+    poly =  PolynomialFeatures(degree = 2)
+    X_poly = poly.fit_transform(X)
+
+    lin2.fit(X_poly, y)
+
+    pred = lin2.predict(poly.fit_transform(X_test))
+    print("Accuracy : " + str(lin2.score(poly.fit_transform(X_test),y_test)))
+    
+
+
 if __name__=="__main__":
-    df = pd.read_csv('../movie_metadata/processed_data.csv')
-    df_train, df_test = train_test_split(df, test_size = 0.2)
-    y_train = df_train["imdb_score"]
-    x_train = df_train.drop(columns = ["imdb_score", "movie_title"])
-    y_test = df_test["imdb_score"]
-    x_test = df_test.drop(columns = ["imdb_score", "movie_title"])
-    linear_regression(x_train, y_train, x_test, y_test)
-    print(df)
+    df = pd.read_csv('movie_metadata/processed_data.csv')
+    df.drop(['Unnamed: 0'], axis= 1, inplace= True)
+    
+    
+    #linear_regression(x_train, y_train, x_test, y_test)
+
+    PolyReg(df)
+    #print(df)
